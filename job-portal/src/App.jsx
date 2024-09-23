6bd3ba29bc399f40536549503a4c888249f9ca29
Kiran9223 <88050617+Kiran9223@users.noreply.github.com>
2024-09-22 21:43:55 -0700

    Init and landing page

---
+++ job-portal/src/App.jsx
@@ -0,0 +1,30 @@
+import Hero from "./components/Hero"
+import HomeCards from "./components/HomeCards"
+import JobListings from "./components/JobListings"
+import Navbar from "./components/Navbar"
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+
+      <Hero title="Test" subtitle="test sub"/>
+
+      <HomeCards/>
+
+      <JobListings/>
+
+    <section className="m-auto max-w-lg my-10 px-6">
+      <a
+        href="jobs.html"
+        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
+    </section>
+
+    <script src="js/main.js"></script>
+    </>
+  )
+}
+
+export default App
